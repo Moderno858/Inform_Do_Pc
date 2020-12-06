e37e6829bbbb240a0b169da974987dbc9abaa581
Moderno <71466911+Moderno858@users.noreply.github.com>
2020-12-05 22:38:14 -0300

    Update Infom.sh

--- Infom.sh
+++ Infom.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "O Home do seu pc é $HOME"
-echo "O Loganame do seu pc é $LOGNAME"
-echo "A liguagem do seu pc é $LANGUAGE"
-
+echo "O Loganame do seu pc e $LOGNAME"
+echo "A liguagem do seu pc e $LANGUAGE"
+echo "A UID do seu pc é $UID"
+echo "O Display do seu pc é $DISPLAY"
+echo "O Windowid do seu pc é $WINDOWID"
+echo "O Colortem do seu pc é $COLORTERM"
 
 
 
